4c4b49ba89cd4f7ce870e428fefd7e5001b92812
Tagir Enikeev <enikeev.tg@gmail.com>
2023-10-23 09:41:45 +0300

    tests: GetPlotPoints() no throw test

--- tests/u_tests_smart_calculator.cc
+++ tests/u_tests_smart_calculator.cc
@@ -3,6 +3,109 @@
 
 namespace e_calc {
 
+/*
+EXCEPTIONS
+*/
+
+TEST_F(CalculatorTests, ThrowTooLargeLength) {
+  std::string expression;
+  for (int i = 0; i < 256; ++i) {
+    expression.push_back('1');
+  }
+  EXPECT_ANY_THROW(controller_->SetCalculator(expression));
+}
+
+TEST_F(CalculatorTests, ThrowIncorrectLastInputToken) {
+  std::string expression{"18+"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowIncorrectInput) {
+  std::string expression{"18+)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowUndefinedToken1) {
+  std::string expression{"expression"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowUndefinedToken2) {
+  std::string expression{"sing(3.14)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowUnbalancedBrackets) {
+  std::string expression{"sin(3.14"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowIncorrectPlotRange1) {
+  std::string expression{"cos(x)"};
+  controller_->SetCalculator(expression);
+  double x_min = -1000001;
+  double x_max = 11;
+  double x_step = 0.01;
+  EXPECT_ANY_THROW(controller_->GetPlotPoints(x_min, x_max, x_step));
+}
+
+TEST_F(CalculatorTests, ThrowIncorrectPlotRange2) {
+  std::string expression{"tan(x)"};
+  controller_->SetCalculator(expression);
+  double x_min = -11;
+  double x_max = 1000001;
+  double x_step = 0.01;
+  EXPECT_ANY_THROW(controller_->GetPlotPoints(x_min, x_max, x_step));
+}
+
+TEST_F(CalculatorTests, ThrowIncorrectPlotRange3) {
+  std::string expression{"sqrt(x)"};
+  controller_->SetCalculator(expression);
+  double x_min = 11;
+  double x_max = -11;
+  double x_step = 0.01;
+  EXPECT_ANY_THROW(controller_->GetPlotPoints(x_min, x_max, x_step));
+}
+
+TEST_F(CalculatorTests, ThrowAcosInvalidArgument) {
+  std::string expression{"acos(1.01)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowAsinInvalidArgument) {
+  std::string expression{"asin(-1.01)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowLnInvalidArgument) {
+  std::string expression{"ln(-0.01)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowLogInvalidArgument) {
+  std::string expression{"log(-0.01)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+TEST_F(CalculatorTests, ThrowSqrtInvalidArgument) {
+  std::string expression{"sqrt(-0.01)"};
+  controller_->SetCalculator(expression);
+  EXPECT_ANY_THROW(controller_->GetResult());
+}
+
+/*
+NORMAL
+*/
+
 TEST_F(CalculatorTests, AdditionOperation) {
   std::string expression{"1563.156 + 694.644"};
   double reference_value = 1563.156 + 694.644;
@@ -96,12 +199,21 @@ TEST_F(CalculatorTests, GetPlotPointsAsin) {
   double x_min = -11;
   double x_max = 11;
   double x_step = 1;
+  EXPECT_NO_THROW(controller_->GetPlotPoints(x_min, x_max, x_step));
+}
+
+TEST_F(CalculatorTests, GetPlotPointsAcos) {
+  std::string expression{"acos(x)"};
+  controller_->SetCalculator(expression);
+  double x_min = -11;
+  double x_max = 11;
+  double x_step = 1;
   e_calc::PlotPoints points = controller_->GetPlotPoints(x_min, x_max, x_step);
   double x = x_min;
   int i = 0;
   while (x <= x_max) {
     if (-1 <= x && x <= 1) {
-      double reference_value = std::asin(x);
+      double reference_value = std::acos(x);
       EXPECT_EQ(points.y_coord[i], reference_value);
       ++i;
     }
